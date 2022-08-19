4a551c3e117511c1a93daa76ddc346c7969f60f6
Charlyeneh <charleschukwunweikeeneh@gmail.com>
2022-08-19 23:37:13 +0100

    Establish connection to the database

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('connection to the database is successful'));
+
+//db connection
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 app.listen(port, () => {
 	console.log(`App is listening on port ${port}!`);
