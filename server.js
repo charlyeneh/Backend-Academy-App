e7ae08683998e10560d258f379513142ac3697d2
Charlyeneh <charleschukwunweikeeneh@gmail.com>
2022-08-21 22:49:32 +0100

    Modify the server.js file to maintain proper versioning

--- server.js
+++ server.js
@@ -3,25 +3,33 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const port = process.env.PORT || 5000;
+const { errorMiddleware } = require('./middlewares/errors');
+const studentsRouter = require('./routes/students');
+
+const classesRouter = require('./routes/subjects');
+
+const port = process.env.PORT || 5001;
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('connection to the database is successful'));
 
 //db connection
-mongoose.connect(process.env.DATABASE_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+mongoose.connect(
+	process.env.DATABASE_URL || 'mongodb://localhost:27017/charly',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
 
 app.use(express.json());
 
-const studentsRouter = require('./routes/students');
+//The API routes
 app.use('/api/v1/students', studentsRouter);
+app.use('/api/v1/subjects', classesRouter);
 
-const classesRouter = require('./routes/subjects');
-app.use('/subjects', classesRouter);
+app.use(errorMiddleware);
 
 app.listen(port, () => {
 	console.log(`App is listening on port ${port}!`);
