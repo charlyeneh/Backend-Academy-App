6fc80d7cc81ef6c92df67f51b22676fdc0db3577
Charlyeneh <charleschukwunweikeeneh@gmail.com>
2022-08-22 21:06:14 +0100

    Instal dependency

--- services/students.js
+++ services/students.js
@@ -2,41 +2,48 @@ const studentModel = require('../models/students');
 const { ApiError } = require('../utils/error');
 
 const generateRegNumber = async () => {
-  const lastStudent = await studentModel.findOne({}).sort({ createdAt: 'desc' }).limit(1);
+	const lastStudent = await studentModel
+		.findOne({})
+		.sort({ createdAt: 'desc' })
+		.limit(1);
 
-  if(!lastStudent){ 
-    return '0001';
-  }
+	if (!lastStudent) {
+		return '0001';
+	}
 
-  const { regNumber } = lastStudent;
+	const { regNumber } = lastStudent;
 
-  return `000${+regNumber + 1}`.slice(-4);
-}
+	return `000${+regNumber + 1}`.slice(-4);
+};
 
 const createStudent = async (data) => {
-  const regNumber = await generateRegNumber();
+	const regNumber = await generateRegNumber();
 
-  return await studentModel.create({ ...data, regNumber });
-}
+	return await studentModel.create({ ...data, regNumber });
+};
 
 const getAllStudents = async (params) => {
-  const { page, size, ...rest } = params;
+	const { page, size, ...rest } = params;
 	const query = { ...rest };
 	const project = {
 		_id: 0,
-  };
+	};
 
-	return await studentModel.find(query, project).skip((page - 1) * size).limit(size).lean();
+	return await studentModel
+		.find(query, project)
+		.skip((page - 1) * size)
+		.limit(size)
+		.lean();
 };
 
 const getStudentById = async (id) => {
-  const project = {
+	const project = {
 		_id: 0,
-  };
-  
+	};
+
 	const student = await studentModel.findOne({ _id: id }, project).lean();
 
-	if(!student) {
+	if (!student) {
 		throw new ApiError(404, `Student with id: ${id} not found`);
 	}
 
@@ -46,12 +53,12 @@ const getStudentById = async (id) => {
 const updateStudent = async (id, data) => {
 	const filter = { _id: id };
 	const update = {
-		$set: { ...data }
-	}
+		$set: { ...data },
+	};
 
 	let student = await getStudentById(id);
 
-	if(!student) {
+	if (!student) {
 		throw new ApiError(409, `Student with id: ${id} does not exist!`);
 	}
 
@@ -60,8 +67,8 @@ const updateStudent = async (id, data) => {
 
 const getStudentsByClass = async (grade, level) => {
 	const filter = {
-    grade,
-    level,
+		grade,
+		level,
 	};
 	const project = { _id: 0 };
 
@@ -79,11 +86,11 @@ const deleteAllStudents = async () => {
 };
 
 module.exports = {
-  getAllStudents,
-  createStudent,
-  getStudentById,
-  updateStudent,
-  getStudentsByClass,
-  deleteStudent,
-  deleteAllStudents,
-}
+	getAllStudents,
+	createStudent,
+	getStudentById,
+	updateStudent,
+	getStudentsByClass,
+	deleteStudent,
+	deleteAllStudents,
+};
